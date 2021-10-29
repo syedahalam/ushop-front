e40189c4282d54ecdd5b1ce2a4bf2cc5d5c496e9
syedahalam <syedahalam@yahoo.com>
2021-10-29 01:32:56 -0400

    add to cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen'
+import CartScreen from './screens/CartScreen'
 import ProductScreen from './screens/ProductScreen'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App() {
 				<Container>
 					<Route path='/' component={HomeScreen} exact/>
 					<Route path='/product/:id' component={ProductScreen} exact/>
+					<Route path='/cart/:id?' component={CartScreen} exact/>
 				</Container>
 			</main>
 			<Footer />
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import products from '../products'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap';
+import Rating from '../components/Rating';
 
-const ProductScreen = ({match}) => {
-    const product = products.find((p)=>p._id === match.params.id)
+import axios from 'axios';
+
+const ProductScreen = ({match, history}) => {
+	const [qty, setQty] = useState(1)
+
+	const [ product, setProduct ] = useState([])
+
+    useEffect(() => {
+    const getProduct = async () =>{
+        const { data } = await axios.get(`/products/${match.params.id}`);
+
+        setProduct(data)
+    }
+      getProduct()
+	},[])
+
+	const addToCart = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`)
+	}
+
+    // const product = products.find((p)=>p._id === match.params.id)
     return (
 			<div>
 				<Link to='/' className='btn btn-light my-3'>
@@ -57,8 +75,35 @@ const ProductScreen = ({match}) => {
 									</Row>
 								</ListGroup.Item>
 
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col xs='auto' className='my-1'>
+											<Form.Control
+												as='select'
+												value={qty}
+												onChange={(e)=>setQty(e.target.value)}
+											>
+												{
+													[...Array(product.countInStock).keys()].map((x)=>(
+														<option key={x+1} value={x+1}>
+															{x+1}
+														</option>
+													))
+												}
+											</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
+
+
+
 								<ListGroup.Item>
-									<Button className='btn-block' type='button' disabled={product.countInStock === 0 }>Add to Cart</Button>
+									<Button 
+									onClick={addToCart}
+									className='btn-block' type='button' disabled={product.countInStock === 0 }>Add to Cart</Button>
 								</ListGroup.Item>
 
 							</ListGroup>

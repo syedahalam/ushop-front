0feac0bbb5f700b9c46217cab0838e997de0bac0
syedahalam <syedahalam@yahoo.com>
2021-10-28 22:57:33 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div>
-     hi from new
-    </div>
-  );
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Route path='/' component={HomeScreen} exact/>
+					<Route path='/product/:id' component={ProductScreen} exact/>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+const Header = () => {
+    return (
+			<header>
+				<Navbar bg='dark' expand='lg' variant='dark' collapseOnSelect>
+					<Container>
+
+						<LinkContainer to='/'>
+							<Navbar.Brand>UShop</Navbar.Brand>
+						</LinkContainer>
+
+						<Navbar.Toggle aria-controls='basic-navbar-nav' />
+						<Navbar.Collapse id='basic-navbar-nav'>
+							<Nav className='mr-auto'>
+
+								<LinkContainer to='/cart'>
+									<Nav.Link>
+										<i className='fas fa-shopping-cart'></i>Cart
+									</Nav.Link>
+								</LinkContainer>
+
+								<LinkContainer to='/login'>
+								<Nav.Link>
+									<i className='fas fa-user'></i>Login
+								</Nav.Link>
+								</LinkContainer>
+								
+							</Nav>
+						</Navbar.Collapse>
+					</Container>
+				</Navbar>
+			</header>
+		);
+}
+
+export default Header

7d843a7bea380cca95a2701012f10cd25765d12f
syedahalam <syedahalam@yahoo.com>
2021-11-04 13:26:26 -0400

    dep

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -3,7 +3,7 @@ import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty)=> async (dispatch, getState)=>{
     const { data } = await axios.get(
-			`https://scary-mummy-74832.herokuapp.com/products/${id}`
+			`https://agile-sea-83094.herokuapp.com/products/${id}`
 		);
 
     dispatch({
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,44 +3,39 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
-
 	PRODUCT_DETAILS_REQUEST,
 	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
 // here thunk is going to make async call
-export const listProducts = () => async(dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_LIST_REQUEST });
-        //make an api call through axios
-        const { data } = await axios.get(
-					'https://scary-mummy-74832.herokuapp.com/products/'
-				);
+export const listProducts = () => async (dispatch) => {
+	try {
+		dispatch({ type: PRODUCT_LIST_REQUEST });
+		//make an api call through axios
+		const { data } = await axios.get(
+			'https://agile-sea-83094.herokuapp.com/products/'
+		);
 
-        dispatch({ type: PRODUCT_LIST_SUCCESS,
-                    payload: data
-                });
-        
-    } catch (error) {
-        dispatch({
-					type: PRODUCT_LIST_FAIL,
-					// error.response && error.response.data.message  -<changed  after user auth in backend
-					payload:
-						error.response && error.response.data.detail
-							? error.response.data.detail
-							: error.message,
-				});
-    }
-}
-
+		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({
+			type: PRODUCT_LIST_FAIL,
+			// error.response && error.response.data.message  -<changed  after user auth in backend
+			payload:
+				error.response && error.response.data.detail
+					? error.response.data.detail
+					: error.message,
+		});
+	}
+};
 
 export const listProductDetails = (id) => async (dispatch) => {
 	try {
 		dispatch({ type: PRODUCT_DETAILS_REQUEST });
 		//make an api call through axios
 		const { data } = await axios.get(
-			`https://scary-mummy-74832.herokuapp.com/products/${id}`
+			`https://agile-sea-83094.herokuapp.com/products/${id}`
 		);
 
 		dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
